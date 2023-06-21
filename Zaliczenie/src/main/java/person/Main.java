f7ee5c60e33ddd7c401a3ed6b1d809233db1337a
WeLento <sb.welento@gmail.com>
2023-06-21 15:07:33 +0200

    Utworzenie 3 klas powiazane ze soba 2 pakietami z klasa testujaca

---
+++ Zaliczenie/src/main/java/person/Main.java
@@ -0,0 +1,25 @@
+package person;
+
+import com.google.gson.Gson;
+import org.example.Person;
+import org.example.Staff;
+import org.example.Student;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("John", "123 Main St");
+        System.out.println(person.toString());
+
+        Student student = new Student("Jane", "456Park Ave", "Computer Science", 3, 5000.0);
+        System.out.println(student.toString());
+
+        Staff staff = new Staff("Bob", "789 Broadway", "School of Engineering", 75000.0);
+        System.out.println(staff.toString());
+
+        Gson gson = new Gson();
+        String personjson = gson.toJson(person);
+        System.out.println(personjson);
+
+    }
+}
